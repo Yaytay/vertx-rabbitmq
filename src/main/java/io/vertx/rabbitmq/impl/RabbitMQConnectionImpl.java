d0ceee5ab797e78cf949b0059f8129eb87a83eba
Yaytay <jim-github@spudsoft.co.uk>
2021-11-14 14:16:46 +0000

    Clear the connecting future when connecting the channel fails.

--- src/main/java/io/vertx/rabbitmq/impl/RabbitMQConnectionImpl.java
+++ src/main/java/io/vertx/rabbitmq/impl/RabbitMQConnectionImpl.java
@@ -387,6 +387,14 @@ public class RabbitMQConnectionImpl implements RabbitMQConnection, ShutdownListe
                     promise.complete(conn.createChannel());
                   } catch(IOException ex) {
                     logger.error("Failed to create channel: ", ex);
+                    synchronized(connectingPromiseLock) {
+                      try {
+                        conn.abort();
+                      } catch(Throwable ex2) {
+                        logger.warn("Failed to abort existing connect (should be harmless): ", ex);
+                      }
+                      connectingFuture = null;
+                    }
                     if (shouldRetryConnection()) {
                       openChannel(lastInstance).onComplete(promise);
                     }
